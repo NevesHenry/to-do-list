f3a6d99c76b2ff83ed30a93a4d5eb4e4ac549a1c
NevesHenry <henriquereisneves@hotmail.com>
2024-08-02 14:54:38 -0300

    Small fix

--- src/main/resources/database/migrations/V1__create-table-task.sql
+++ src/main/resources/database/migrations/V1__create-table-task.sql
@@ -1,7 +1,7 @@
 CREATE TABLE task (
     id INT PRIMARY KEY UNIQUE,
     title VARCHAR(255) NOT NULL,
-    description TEXT,
+    description VARCHAR(255),
     status  VARCHAR(15) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_active TINYINT(1) NOT NULL
