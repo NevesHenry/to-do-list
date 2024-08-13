0c396f8eb8850fbf99d55447e9830be5eda610a2
NevesHenry <henriquereisneves@hotmail.com>
2024-08-13 14:29:59 -0300

    Add token validation

--- src/main/java/com/example/to_do_list/infra/security/SecurityConfigurations.java
+++ src/main/java/com/example/to_do_list/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.example.to_do_list.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter SecurityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -28,6 +33,8 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/tasks").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(SecurityFilter,
+                        UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
