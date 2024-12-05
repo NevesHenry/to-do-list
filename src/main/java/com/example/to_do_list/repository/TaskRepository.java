b4a78a4a307ed97eef13720d4e848bc458e021ca
NevesHenry <henriquereisneves@hotmail.com>
2024-12-04 23:43:43 -0300

    Add Swagger documentation

--- src/main/java/com/example/to_do_list/repository/TaskRepository.java
+++ src/main/java/com/example/to_do_list/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package com.example.to_do_list.repository;
 
 import com.example.to_do_list.domain.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
     List<Task> findAllByIsActiveTrue();
 }
--- src/main/java/com/example/to_do_list/repository/UserRepository.java
+++ src/main/java/com/example/to_do_list/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.example.to_do_list.repository;
 import com.example.to_do_list.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     UserDetails findByUsername(String username);
     List<User> findAllByIsActiveTrue();
